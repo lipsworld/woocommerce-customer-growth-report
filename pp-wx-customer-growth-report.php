b5aee24bfffbda294eef2aa6a7ad489eeddbab35
Alan Ng <jenfoong@gmail.com>
2014-07-18 17:14:48 +0800

    Add Updater

--- pp-wx-customer-growth-report.php
+++ pp-wx-customer-growth-report.php
@@ -19,9 +19,21 @@ if (!class_exists('Wx_Admin_Report')) {
 }
 
 require_once( 'classes/class-pp-wx-customer-growth-report.php' );
-
+require_once( 'classes/class-pootlepress-updater.php');
 
 $GLOBALS['pootlepress_wx_customer_growth_report'] = new Pootlepress_Wx_Customer_Growth_Report( __FILE__ );
 $GLOBALS['pootlepress_wx_customer_growth_report']->version = '1.0.0';
 
+add_action('init', 'pp_wcgr_updater');
+function pp_wcgr_updater()
+{
+    if (!function_exists('get_plugin_data')) {
+        include(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
+    $data = get_plugin_data(__FILE__);
+    $wptuts_plugin_current_version = $data['Version'];
+    $wptuts_plugin_remote_path = 'http://www.pootlepress.com/?updater=1';
+    $wptuts_plugin_slug = plugin_basename(__FILE__);
+    new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
+}
 ?>
